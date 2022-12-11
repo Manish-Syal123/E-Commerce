8cc1b72f0db0c0473db855018e5cfbab3f07a0c1
Manish_Syal <manishsyal77@gmail.com>
2022-12-11 19:42:28 +0530

    Used Styled components

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const Home = () => {
   return (
-    <div>
-      Home Page
-    </div>
+    <Wrapper className='test'>Home Page</Wrapper>
   )
-}
+};
 
-export default Home
+// div is converted/stored to a variable called wrapper & where wraper has its own styles property
+const Wrapper = styled.section` 
+ background-color: ${({ theme }) => theme.colors.bg};
+ width: 20rem;
+ height: 20rem;
+`; 
+
+export default Home;
