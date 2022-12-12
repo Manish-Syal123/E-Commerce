acb7f94e1f19cf1b487bd96e5e3a9b9847dea7f4
Manish_Syal <manishsyal77@gmail.com>
2022-12-13 00:18:05 +0530

    Menu icon with its page made Responsive

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,13 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 const Home = () => {
-  return (
-    <Wrapper className='test'>Home Page</Wrapper>
-  )
+  return <Wrapper className="test">Home</Wrapper>;
 };
 
-// div is converted/stored to a variable called wrapper & where wraper has its own styles property
-const Wrapper = styled.section` 
- background-color: ${({ theme }) => theme.colors.bg};
- width: 20rem;
- height: 20rem;
-`; 
+const Wrapper = styled.section`
+  height: 100vh;
+  background-color: ${({ theme }) => theme.colors.bg};
+`;
 
 export default Home;
