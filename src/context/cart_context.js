445f35536a815589b89a14c3c99b3e051eaebc5d
Manish_Syal <manishsyal77@gmail.com>
2023-02-24 12:19:04 +0530

    Stored cart data into localStorage

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -1,9 +1,19 @@
-import { createContext, useContext, useReducer } from "react";
+import { createContext, useContext, useEffect, useReducer } from "react";
 import reducer from "../reducer/cartReducer";
 const CartContext = createContext();
 
+const getLocalCartData = () => {
+  let localCartData = localStorage.getItem("ManishCart"); //before refreshing the page we are storing the data in a variable.
+  if (localCartData === []) {
+    return [];
+  } else {
+    return JSON.parse(localCartData);
+  }
+};
+
 const initialState = {
-  cart: [],
+  //cart: [],
+  cart: getLocalCartData(),
   total_item: "",
   total_amount: "",
   shipping_fee: 50000,
@@ -18,10 +28,15 @@ const CartProvider = ({ children }) => {
   };
 
   // to remove item from the cart
-
   const removeItem = (id) => {
     dispatch({ type: "REMOVE_ITEM", payload: id });
   };
+
+  //to add the added cart item data in localStorage
+  // get=> toget the data ...Vs... set=> to set the data
+  useEffect(() => {
+    localStorage.setItem("ManishCart", JSON.stringify(state.cart));
+  }, [state.cart]);
 
   return (
     <CartContext.Provider value={{ ...state, addToCart, removeItem }}>
